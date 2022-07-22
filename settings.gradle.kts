64c7d34c18e751d273784a904a6e976843f1e9dc
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2022-07-22 16:24:43 -0400

    Revert "testing 0.9.12 fix with mavenCentral()"
    This reverts commit b5108aaac8d3e5390bc72d507f0c4e76b5aa9651.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
-        mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
     }
