046015de28a21b55bdef69117b7dd2060415a1bb
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2022-07-22 14:51:09 -0400

    Revert "ugraded to 0.9.13-SNAPSHOT"
    This reverts commit 577617ff5c52de573cc3cb47befcb17ceb83cfad.

--- src/jsMain/kotlin/io/github/dennistsar/sirs_kobweb/pages/About.kt
+++ src/jsMain/kotlin/io/github/dennistsar/sirs_kobweb/pages/About.kt
@@ -3,7 +3,7 @@ package io.github.dennistsar.sirs_kobweb.pages
 import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.core.Page
 import com.varabyte.kobweb.silk.components.navigation.Link
-import com.varabyte.kobweb.silk.components.text.SpanText
+import com.varabyte.kobweb.silk.components.text.Text
 import io.github.dennistsar.sirs_kobweb.components.layouts.PageLayout
 import org.jetbrains.compose.web.dom.P
 
@@ -11,7 +11,7 @@ import org.jetbrains.compose.web.dom.P
 @Composable
 fun AboutPage() {
     PageLayout("ABOUT") {
-        SpanText("This is a skeleton app used to showcase a basic site made using Kobweb")
+        Text("This is a skeleton app used to showcase a basic site made using Kobweb")
         P()
         Link("/", "Go Home")
     }
--- src/jsMain/kotlin/io/github/dennistsar/sirs_kobweb/pages/Index.kt
+++ src/jsMain/kotlin/io/github/dennistsar/sirs_kobweb/pages/Index.kt
@@ -2,7 +2,7 @@ package io.github.dennistsar.sirs_kobweb.pages
 
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.core.Page
-import com.varabyte.kobweb.silk.components.text.SpanText
+import com.varabyte.kobweb.silk.components.text.Text
 import io.github.dennistsar.sirs_kobweb.components.layouts.PageLayout
 import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.dom.Input
@@ -12,7 +12,7 @@ import org.jetbrains.compose.web.dom.P
 @Composable
 fun HomePage() {
     PageLayout("Welcome to Kobweb 2.0!") {
-        SpanText("Please enter your name")
+        Text("Please enter your name")
         var name by remember { mutableStateOf("") }
         Input(
             InputType.Text,
@@ -21,7 +21,7 @@ fun HomePage() {
             }
         )
         P()
-        SpanText("Hello ${name.takeIf { it.isNotBlank() } ?: "World"}!")
+        Text("Hello ${name.takeIf { it.isNotBlank() } ?: "World"}!")
     }
 
 }
